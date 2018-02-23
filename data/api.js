2ef854372136331574d21c2e7193cfe11325e358
Tomasz Ducin <tomasz.ducin@gmail.com>
2018-02-23 23:41:28 +0100

    additional data

--- data/api.js
+++ data/api.js
@@ -1,24 +1,24 @@
 const API = (function(){
-  
-  const timeout = () => Math.random() * 100
+	
+	const timeout = () => Math.random() * 100
 
-  const wrapWithPromise = fn =>
-    (...args) => {
-      return new Promise((resolve, reject) =>
-        setTimeout(() => resolve(fn(...args)), timeout())
-      )
-    }
+	const wrapWithPromise = fn =>
+		(...args) => {
+			return new Promise((resolve, reject) =>
+				setTimeout(() => resolve(fn(...args)), timeout())
+			)
+		}
 
-  return {
-    // getTodoById(id): thenable
-    getTodoById: wrapWithPromise(db.getTodoById),
-    // getTodos(id): thenable
-    getTodos: wrapWithPromise(db.getTodos),
-    // getEmployee(id): thenable
-    getEmployee: wrapWithPromise(db.getEmployeeById),
-    // getEmployeesByNationality(nationality): thenable
-    getEmployeesByNationality: wrapWithPromise(db.getEmployeesByNationality),
-    // getNationalities(): thenable
-    getNationalities: wrapWithPromise(db.getNationalities),
-  }
+	return {
+		// getTodoById(id): thenable
+		getTodoById: wrapWithPromise(db.getTodoById),
+		// getTodos(id): thenable
+		getTodos: wrapWithPromise(db.getTodos),
+		// getEmployee(id): thenable
+		getEmployee: wrapWithPromise(db.getEmployeeById),
+		// getEmployeesByNationality(nationality): thenable
+		getEmployeesByNationality: wrapWithPromise(db.getEmployeesByNationality),
+		// getNationalities(): thenable
+		getNationalities: wrapWithPromise(db.getNationalities),
+	}
 }());
